938dd9b399f81c3c95ce43ea01a76f4855facebb
alpenfritz <mymailismissing@gmail.com>
2018-04-09 21:41:04 +0900

    Add state + testing to Gift component

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -8,4 +8,8 @@ describe('Gift', () => {
   it('renders correctly', () => {
     expect(gift).toMatchSnapshot();
   });
+
+  it('initializes a person and present in `state`', () => {
+    expect(gift.state()).toEqual({ person: '', present: '' });
+  })
 });
